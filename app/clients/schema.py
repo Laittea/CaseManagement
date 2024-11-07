2bc069888cc54201687ff1a61bb0556d393c7862
jiayangliu <liujiayang_ljy@163.com>
2024-11-06 17:01:14 -0800

    refactor: data type conversion

--- app/clients/schema.py
+++ app/clients/schema.py
@@ -1,8 +1,6 @@
-from pydantic import BaseModel, Field, field_serializer
-
+from pydantic import BaseModel, Field, field_serializer, model_serializer
 
 class PredictionInput(BaseModel):
-    # TODO: keep the field order the same as in model.py by reading a config file
     """
     PredictionInput is the validated input from webpage users
 
@@ -18,37 +16,117 @@ class PredictionInput(BaseModel):
     work_experience: int = Field(..., validation_alias='work_experience', serialization_alias='work_experience')
     canada_workex: int = Field(..., validation_alias='canada_workex', serialization_alias='canada_workex')
     dep_num: int = Field(..., validation_alias='dep_num', serialization_alias='dep_num')
-    canada_born: str = Field(..., validation_alias='canada_born', serialization_alias='canada_born')
+    canada_born: bool = Field(..., validation_alias='canada_born', serialization_alias='canada_born')
     citizen_status: str = Field(..., validation_alias='citizen_status', serialization_alias='citizen_status')
     level_of_schooling: str = Field(..., validation_alias='level_of_schooling', serialization_alias='level_of_schooling')
-    fluent_english: str = Field(..., validation_alias='fluent_english', serialization_alias='fluent_english')
+    fluent_english: bool = Field(..., validation_alias='fluent_english', serialization_alias='fluent_english')
     reading_english_scale: int = Field(..., validation_alias='reading_english_scale', serialization_alias='reading_english_scale')
     speaking_english_scale: int = Field(..., validation_alias='speaking_english_scale', serialization_alias='speaking_english_scale')
     writing_english_scale: int = Field(..., validation_alias='writing_english_scale', serialization_alias='writing_english_scale')
     numeracy_scale: int = Field(..., validation_alias='numeracy_scale', serialization_alias='numeracy_scale')
     computer_scale: int = Field(..., validation_alias='computer_scale', serialization_alias='computer_scale')
-    transportation_bool: str = Field(..., validation_alias='transportation_bool', serialization_alias='transportation_bool')
-    caregiver_bool: str = Field(..., validation_alias='caregiver_bool', serialization_alias='caregiver_bool')
+    transportation_bool: bool = Field(..., validation_alias='transportation_bool', serialization_alias='transportation_bool')
+    caregiver_bool: bool = Field(..., validation_alias='caregiver_bool', serialization_alias='caregiver_bool')
     housing: str = Field(..., validation_alias='housing', serialization_alias='housing')
     income_source: str = Field(..., validation_alias='income_source', serialization_alias='income_source')
-    felony_bool: str = Field(..., validation_alias='felony_bool', serialization_alias='felony_bool')
-    attending_school: str = Field(..., validation_alias='attending_school', serialization_alias='attending_school')
-    currently_employed: str = Field(..., validation_alias='currently_employed', serialization_alias='currently_employed')
-    substance_use: str = Field(..., validation_alias='substance_use', serialization_alias='substance_use')
+    felony_bool: bool = Field(..., validation_alias='felony_bool', serialization_alias='felony_bool')
+    attending_school: bool = Field(..., validation_alias='attending_school', serialization_alias='attending_school')
+    currently_employed: bool = Field(..., validation_alias='currently_employed', serialization_alias='currently_employed')
+    substance_use: bool = Field(..., validation_alias='substance_use', serialization_alias='substance_use')
     time_unemployed: int = Field(..., validation_alias='time_unemployed', serialization_alias='time_unemployed')
-    need_mental_health_support_bool: str = Field(..., validation_alias='need_mental_health_support_bool', serialization_alias='need_mental_health_support_bool')
+    need_mental_health_support_bool: bool = Field(..., validation_alias='need_mental_health_support_bool', serialization_alias='need_mental_health_support_bool')
 
-    @field_serializer('gender')
-    def serialize_age(self, gender: str):
-        return 1 if gender == 'M' else 2
+    # The following field serializer converts specific fields into numerical types,
+    @field_serializer('income_source')
+    def serialize_income_source(self, income_source: str):
+        match income_source:
+            case 'No Source of Income':
+                return 1
+            case 'Employment Insurance':
+                return 2
+            case 'Workplace Safety and Insurance Board':
+                return 3
+            case 'Ontario Works applied or receiving':
+                return 4
+            case 'Ontario Disability Support Program applied or receiving':
+                return 5
+            case 'Dependent of someone receiving OW or ODSP':
+                return 6
+            case 'Crown Ward':
+                return 7
+            case 'Employment':
+                return 8
+            case 'Self-Employment':
+                return 9
+            case 'Other (specify)':
+                return 10
 
-    @field_serializer('canada_born')
-    def serialize_canada_born(self, canada_born: str):
-        return 1 if canada_born == 'true' else 0
+    @field_serializer('housing')
+    def serialize_housing(self, housing: str):
+        match housing:
+            case 'Renting-private':
+                return 1
+            case 'Renting-subsidized':
+                return 2
+            case 'Boarding or lodging':
+                return 3
+            case 'Homeowner':
+                return 4
+            case 'Living with family/friend':
+                return 5
+            case 'Institution':
+                return 6
+            case 'Temporary second residence':
+                return 7
+            case 'Band-owned home':
+                return 8
+            case 'Homeless or transient':
+                return 9
+            case 'Emergency hostel':
+                return 10
+
+    @field_serializer('level_of_schooling')
+    def serialize_level_of_schooling(self, level_of_schooling: str):
+        match level_of_schooling:
+            case 'Grade 0-8':
+                return 1
+            case 'Grade 9':
+                return 2
+            case 'Grade 10':
+                return 3
+            case 'Grade 11':
+                return 4
+            case 'Grade 12 or equivalent':
+                return 5
+            case 'OAC or Grade 13':
+                return 6
+            case 'Some college':
+                return 7
+            case 'Some university':
+                return 8
+            case 'Some apprenticeship':
+                return 9
+            case 'Certificate of Apprenticeship':
+                return 10
+            case 'Journeyperson':
+                return 11
+            case 'Certificate/Diploma':
+                return 12
+            case 'Bachelor’s degree':
+                return 13
+            case 'Post graduate':
+                return 14
 
     @field_serializer('citizen_status')
     def serialize_citizen_status(self, citizen_status: str):
         match citizen_status:
-            case 'citizen': return 0
-            case 'permanent_resident': return 1
-            case 'temporary_resident': return 2
+            case 'citizen':
+                return 0
+            case 'permanent_resident':
+                return 1
+            case 'temporary_resident':
+                return 2
+
+    @field_serializer('gender')
+    def serialize_age(self, gender: str):
+        return 1 if gender == 'M' else 2
--- app/clients/service/logic.py
+++ app/clients/service/logic.py
@@ -13,12 +13,12 @@ column_intervention = [
     'General Employment Assistance Services',
     'Retention Services',
     'Specialized Services',
-    'Employment-Related Financial Supports for Job Seekers and Employers', 
+    'Employment-Related Financial Supports for Job Seekers and Employers',
     'Employer Financial Supports',
     'Enhanced Referrals for Skills Development'
 ]
 
-#loads the model into logic
+# loads the model into logic
 
 import os
 
@@ -27,162 +27,57 @@ filename = os.path.join(current_dir, 'model.pkl')
 model = pickle.load(open(filename, "rb"))
 
 
+def convert_none_bool(value):
+    """
+    convert None to 0, True to 1, False to 0
+    """
+    if value is None:
+        return 0
+    if type(value) == bool:
+        return 1 if value is True else 0
+    else:
+        return value
+
+
 def clean_input_data(data):
-    #translate input into wahtever we trained the model on, numerical data in a specific order
-    columns = ["age","gender","work_experience","canada_workex","dep_num",	"canada_born",	
-               "citizen_status",	"level_of_schooling",	"fluent_english",	"reading_english_scale",	
-               "speaking_english_scale",	"writing_english_scale",	"numeracy_scale",	"computer_scale",	
-               "transportation_bool",	"caregiver_bool",	"housing",	"income_source",	"felony_bool",	"attending_school",	
-               "currently_employed",	"substance_use",	"time_unemployed",	"need_mental_health_support_bool"]
-    demographics = {
-        'age': data['age'],
-        'gender': data['gender'],
-        'work_experience': data['work_experience'],
-        'canada_workex': data['canada_workex'],
-        'dep_num': data['dep_num'],
-        'canada_born': data['canada_born'],
-        'citizen_status': data['citizen_status'],
-        'level_of_schooling': data['level_of_schooling'],
-        'fluent_english': data['fluent_english'],
-        'reading_english_scale': data['reading_english_scale'],
-        'speaking_english_scale': data['speaking_english_scale'],
-        'writing_english_scale': data['writing_english_scale'],
-        'numeracy_scale': data['numeracy_scale'],
-        'computer_scale': data['computer_scale'],
-        'transportation_bool': data['transportation_bool'],
-        'caregiver_bool': data['caregiver_bool'],
-        'housing': data['housing'],
-        'income_source': data['income_source'],
-        'felony_bool': data['felony_bool'],
-        'attending_school': data['attending_school'],
-        'currently_employed': data['currently_employed'],
-        'substance_use': data['substance_use'],
-        'time_unemployed': data['time_unemployed'],
-        'need_mental_health_support_bool': data['need_mental_health_support_bool']
-    }
-    output = []
-    for column in columns:
-        data = demographics.get(column, None) #default is None, and if you want to pass a value, can return any value
-        if isinstance(data, str):
-            data = convert_text(column, data)
-        output.append(data)
+    # TODO: keep the field order the same as in model.py by reading a config file
+    output = [convert_none_bool(v) for v in data.values()]
     return output
 
 
-def clean_input_data_refactor(data):
-    # translate input into whatever we trained the model on, numerical data in a specific order
-    output = []
-    for k, v in data.items():
-        if isinstance(v, str):
-            v = convert_text(k, v)
-        output.append(v)
-    return output
-
-
-def convert_text(column, data:str):
-    # Convert text answers from front end into digits
-    # TODO: ensure that categorical columns match the valid answers in FormNew.jsx (L131)
-    categorical_cols_integers = [
-        {
-            "": 0,
-            "true": 1,
-            "false": 0,
-            "no": 0,
-            "yes": 1,
-            "No": 0,
-            "Yes": 1
-        },
-        {
-            'Grade 0-8': 1,
-            'Grade 9': 2,
-            'Grade 10': 3,
-            'Grade 11': 4,
-            'Grade 12 or equivalent': 5,
-            'OAC or Grade 13': 6,
-            'Some college': 7,
-            'Some university': 8,
-            'Some apprenticeship': 9,
-            'Certificate of Apprenticeship': 10,
-            'Journeyperson': 11,
-            'Certificate/Diploma': 12,
-            'Bachelor’s degree': 13,
-            'Post graduate': 14
-        },
-        {
-            'Renting-private': 1,
-            'Renting-subsidized': 2,
-            'Boarding or lodging': 3,
-            'Homeowner': 4,
-            'Living with family/friend': 5,
-            'Institution': 6,
-            'Temporary second residence': 7,
-            'Band-owned home': 8,
-            'Homeless or transient': 9,
-            'Emergency hostel': 10
-        },
-        {
-            'No Source of Income': 1,
-            'Employment Insurance': 2,
-            'Workplace Safety and Insurance Board': 3,
-            'Ontario Works applied or receiving': 4,
-            'Ontario Disability Support Program applied or receiving': 5,
-            'Dependent of someone receiving OW or ODSP': 6,
-            'Crown Ward': 7,
-            'Employment': 8,
-            'Self-Employment': 9,
-            'Other (specify)': 10
-        },
-        # gender
-        {
-            'M': 1,
-            'F': 2
-        },
-        # citizen
-        {
-            'citizen': 0,
-            'permanent_resident': 1,
-            'temporary_resident': 2
-        }
-    ]
-    for category in categorical_cols_integers:
-        print(f"data: {data}")
-        print(f"column: {column}")
-        if data in category:
-            return category[data]
-
-    if isinstance(data, str) and data.isnumeric():
-        return int(data)
-
-    return data
-
-#creates 128 possible combinations in order to run every possibility through model
+# creates 128 possible combinations in order to run every possibility through model
 def create_matrix(row):
-    data = [row.copy() for _ in range(128)] 
+    data = [row.copy() for _ in range(128)]
     perms = intervention_permutations(7)
     data = np.array(data)
     perms = np.array(perms)
-    matrix = np.concatenate((data,perms), axis = 1) 
+    matrix = np.concatenate((data, perms), axis=1)
     return np.array(matrix)
-#create matrix of permutations of 1 and 0 of num length
+
+
+# create matrix of permutations of 1 and 0 of num length
 def intervention_permutations(num):
-    perms = list(product([0,1],repeat=num))
+    perms = list(product([0, 1], repeat=num))
     return np.array(perms)
+
 
 def get_baseline_row(row):
     print(type(row))
-    base_interventions = np.array([0]*7) # no interventions
+    base_interventions = np.array([0] * 7)  # no interventions
     row = np.array(row)
     print(row)
     print(type(row))
-    line = np.concatenate((row,base_interventions))
+    line = np.concatenate((row, base_interventions))
     return line
+
 
 def intervention_row_to_names(row):
     names = []
     for i, value in enumerate(row):
-        if value == 1: 
+        if value == 1:
             names.append(column_intervention[i])
     return names
+
 
 def process_results(baseline, results):
     ##Example:
@@ -196,73 +91,45 @@ def process_results(baseline, results):
         ]
     }
     """
-    result_list= []
+    result_list = []
     for row in results:
-        percent = row[-1] 
+        percent = row[-1]
         names = intervention_row_to_names(row)
-        result_list.append((percent,names))
+        result_list.append((percent, names))
 
     output = {
-        "baseline": baseline[-1], #if it's an array, want the value inside of the array
+        "baseline": baseline[-1],  # if it's an array, want the value inside of the array
         "interventions": result_list,
     }
     return output
 
+
 def interpret_and_calculate(data):
-    # raw_data = clean_input_data(data)
-    raw_data = clean_input_data_refactor(data)
+    raw_data = clean_input_data(data)
     baseline_row = get_baseline_row(raw_data)
     baseline_row = baseline_row.reshape(1, -1)
-    print("BASELINE ROW IS",baseline_row)
+    print("BASELINE ROW IS", baseline_row)
     intervention_rows = create_matrix(raw_data)
     baseline_prediction = model.predict(baseline_row)
     intervention_predictions = model.predict(intervention_rows)
-    intervention_predictions = intervention_predictions.reshape(-1, 1) #want shape to be a vertical column, not a row
-    result_matrix = np.concatenate((intervention_rows,intervention_predictions), axis = 1) ##CHANGED AXIS
-    
+    intervention_predictions = intervention_predictions.reshape(-1, 1)  # want shape to be a vertical column, not a row
+    result_matrix = np.concatenate((intervention_rows, intervention_predictions), axis=1)  ##CHANGED AXIS
+
     # sort this matrix based on prediction
     # print("RESULT SAMPLE::", result_matrix[:5])
-    result_order = result_matrix[:,-1].argsort() #take all rows and only last column, gives back list of indexes sorted
-    result_matrix = result_matrix[result_order] #indexing the matrix by the order
+    result_order = result_matrix[:, -1].argsort()  # take all rows and only last column, gives back list of indexes sorted
+    result_matrix = result_matrix[result_order]  # indexing the matrix by the order
 
     # slice matrix to only top N results
-    result_matrix = result_matrix[-3:,-8:] #-8 for interventions and prediction, want top 3, 3 combinations of intervention
+    result_matrix = result_matrix[-3:, -8:]  # -8 for interventions and prediction, want top 3, 3 combinations of intervention
     # post process results if needed ie make list of names for each row
-    results = process_results(baseline_prediction,result_matrix)
+    results = process_results(baseline_prediction, result_matrix)
     # build output dict
     print(f"RESULTS: {results}")
     return results
 
 
 #################### Test Data and Methods ####################
-
-# validated dict data for test
-test_data = {
-        "age": "23",
-        "gender": "1",
-        "work_experience": "1",
-        "canada_workex": "1",
-        "dep_num": "0",
-        "canada_born": "1",
-        "citizen_status": "2",
-        "level_of_schooling": "2",
-        "fluent_english": "3",
-        "reading_english_scale": "2",
-        "speaking_english_scale": "2",
-        "writing_english_scale": "3",
-        "numeracy_scale": "2",
-        "computer_scale": "3",
-        "transportation_bool": "2",
-        "caregiver_bool": "1",
-        "housing": "1",
-        "income_source": "5",
-        "felony_bool": "1",
-        "attending_school": "0",
-        "currently_employed": "1",
-        "substance_use": "1",
-        "time_unemployed": "1",
-        "need_mental_health_support_bool": "1"
-    }
 
 # raw data from front end
 test_model = {
@@ -292,35 +159,55 @@ test_model = {
     "need_mental_health_support_bool": "false"
 }
 
+# validated dict data for test (unconverted)
+test_data = {'age': 18, 'gender': 1, 'work_experience': 3, 'canada_workex': 0, 'dep_num': 1, 'canada_born': 1,
+             'citizen_status': 0, 'level_of_schooling': 'Grade 12 or equivalent', 'fluent_english': 'true',
+             'reading_english_scale': 3, 'speaking_english_scale': 1, 'writing_english_scale': 3, 'numeracy_scale': 0,
+             'computer_scale': 2, 'transportation_bool': 'false', 'caregiver_bool': 'true',
+             'housing': 'Living with family/friend', 'income_source': 'No Source of Income', 'felony_bool': 'true',
+             'attending_school': 'false', 'currently_employed': 'true', 'substance_use': 'true', 'time_unemployed': 1,
+             'need_mental_health_support_bool': 'false'}
 
-def test_clean_input_data():
-    print("\n#################### test_clean_input_data() ####################")
-    # the original output data is derived from test_data
-    output_origin = [23, 1, 1, 1, 0, 1, 2, 2, 3, 2, 2, 3, 2, 3, 2, 1, 1, 5, 1, 0, 1, 1, 1, 1]
-    output_refactor = clean_input_data_refactor(test_data)
-    l1, l2 = len(output_origin), len(output_refactor)
-    if l1 != l2:
+# output after data converting
+test_output = [18, 1, 3, 0, 1, 1, 0, 5, 1, 3, 1, 3, 0, 2, 0, 1, 5, 1, 1, 0, 1, 1, 1, 0]
+
+# prediction result
+test_prediction_result = {
+    "baseline": 67.6,
+    "interventions": [
+        (68.7, ["Life Stabilization", "General Employment Assistance Services", "Specialized Services", "Employment-Related Financial Supports for Job Seekers and Employers"]),
+        (68.7, ["Life Stabilization", "Specialized Services", "Employment-Related Financial Supports for Job Seekers and Employers"]),
+        (69.0, ["Life Stabilization", "Specialized Services"])]
+}
+
+
+def test_data_type_conversion():
+    """test the conversion of diff data types"""
+    print("\n#################### test_data_type_conversion() ####################")
+    model_refactor = PredictionInput(**test_model)
+    output_refactor = model_refactor.model_dump(by_alias=True)
+    output_refactor = [v for v in output_refactor.values()]
+
+    if len(test_output) != len(output_refactor):
         print("FAIL: len not equals\n")
         return
-    for i in range(l1):
-        origin, refactor = output_origin[i], output_refactor[i]
-        if type(origin) != type(refactor) or origin != refactor:
-            print("FAIL: the {} th element not equals. origin:{}, refactor:{}\n".format(i, origin, refactor))
+
+    for i in range(len(test_output)):
+        if test_output[i] != output_refactor[i]:
+            print("FAIL: the {} th element not equals. origin:{}, refactor:{}\n".format(i, test_output[i], output_refactor[i]))
             return
     print("PASS\n")
 
 
-def test_alias():
-    print("\n#################### test_alias() ####################")
+def test_prediction():
+    """test the whole prediction process"""
+    print("\n#################### test_prediction() ####################")
     model_refactor = PredictionInput(**test_model)
-    data_refactor = model_refactor.model_dump(by_alias=True)
-    print()
-    print(data_refactor)
-
-
-if __name__ == "__main__":
-    print("running")
-    # print(data)
-    results = interpret_and_calculate(test_data)
-    print(results)
+    output_refactor = model_refactor.model_dump(by_alias=True)
+    result = interpret_and_calculate(output_refactor)
+    # print(result)
+    if result == test_prediction_result:
+        print("PASS\n")
+    else:
+        print("FAIL\n")
 
