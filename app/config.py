54ab6dc76a35ab6eca6379c0afa412dd3e3cc101
Itsc2y <c2ymonica@gmail.com>
2024-11-23 20:26:33 -0800

    fix pylint errors

--- app/config.py
+++ app/config.py
@@ -1,10 +1,30 @@
+"""
+This module contains the configuration settings for the application.
+It uses Pydantic's BaseSettings to load settings from environment variables.
+"""
+
 from pydantic_settings import BaseSettings
+from pydantic import BaseModel
 
 class Settings(BaseSettings):
+    """
+    Settings class to manage application configuration.
+
+    Attributes:
+        MONGODB_URI (str): MongoDB connection URI.
+        MONGODB_NAME (str): MongoDB database name.
+    """
     MONGODB_URI: str
     MONGODB_NAME: str
 
-    class Config:
+    class Config(BaseModel):
+        """
+        Config class to specify Pydantic settings behavior.
+
+        Attributes:
+            env_file (str): Path to the .env file.
+        """
         env_file = ".env"
 
+# Instantiate the settings object to be used throughout the application
 settings = Settings()
