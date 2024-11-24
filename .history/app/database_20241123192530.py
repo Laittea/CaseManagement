345b243b5d1af847395a96e61f960265adb965ca
王天洁 <wangtianjie@wangtianjiedeMacBook-Air.local>
2024-11-23 19:37:47 -0800

    Updating schema and database

--- .history/app/database_20241123192530.py
+++
@@ -1,8 +0,0 @@
-# Database configuration and connection setup using Motor.
-
-from motor.motor_asyncio import AsyncIOMotorClient
-from app.config import settings
-
-client = AsyncIOMotorClient(settings.MONGODB_URI)
-database = client[settings.MONGODB_NAME]
-clients_collection = database.get_collection("clients")
