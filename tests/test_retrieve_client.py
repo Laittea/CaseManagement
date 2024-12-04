d4348ef4d81ac16448be1e546fe0980e4289a7e8
Guotong Liao <815190191@qq.com>
2024-12-03 16:16:59 -0800

    fix: tests

--- tests/test_retrieve_client.py
+++ tests/test_retrieve_client.py
@@ -1,13 +1,14 @@
 """Test functions for the retrieve api"""
 import pytest
 from fastapi import HTTPException
-from app.clients.service.retrieve import retrieve_client, check_valid_input
+from app.clients.service.retrieve import retrieve_client, check_valid_input, search_clients
+from app.clients.service.update_model import ClientUpdateModel
 
 # Retrieve Client Tests
 @pytest.mark.asyncio
 async def test_retrieve_client_success():
     """Test successful retrieval of a client."""
-    client_id = "123"  # Assuming this is a valid client ID in your test database
+    client_id = "123"
 
     result = await retrieve_client(client_id)
 
@@ -15,6 +16,21 @@ async def test_retrieve_client_success():
     assert isinstance(result, dict)
     assert "client_id" in result
     assert result["client_id"] == int(client_id)
+
+# Retrieve Client Tests
+@pytest.mark.asyncio
+async def test_search_client_success():
+    """Test successful retrieval of a client."""
+    client_data = ClientUpdateModel(
+        age=30,
+        housing="None"
+    )
+
+    with pytest.raises(HTTPException) as excinfo:
+        await search_clients(client_data)
+
+    # Assertions
+    assert excinfo.value.status_code == 404
 
 @pytest.mark.asyncio
 async def test_retrieve_client_not_found():
