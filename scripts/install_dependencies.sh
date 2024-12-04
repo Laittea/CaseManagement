687576ffa86ea249f28c8f57d8dc3530aa0ebcb7
Xiaoxi Yang <yang.xiaoxi1@northeastern.edu>
2024-12-03 18:11:59 -0800

    update appspec.yml and scripts

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
-sudo apt-get install -y docker-ce
-sudo systemctl start docker
-sudo systemctl enable docker
-sudo usermod -aG docker ubuntu
+# Update package lists
+sudo apt update -y
+# Install Apache (httpd equivalent)
+sudo apt install -y apache2
